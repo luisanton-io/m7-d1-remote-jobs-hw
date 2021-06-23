961d99644443fa23cec978f184acdff87eb5e87f
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2021-06-23 20:53:09 +0200

    added redux

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MainSearch from './components/MainSearch';
 import CompanySearchResults from './components/CompanySearchResults';
-import { BrowserRouter, Route } from 'react-router-dom'
+import Favourites from './components/Favourites';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import store from './store';
+import { Provider } from 'react-redux'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route exact path='/' component={MainSearch} />
-      <Route exact path='/:companyName' component={CompanySearchResults} />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={MainSearch} />
+          <Route exact path='/favourites' component={Favourites} />
+          <Route exact path='/:companyName' component={CompanySearchResults} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
